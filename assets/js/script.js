13f0961bd2a145fc47d24dc805425127784b71bb
Max McDonough <max.mcdonough@gmail.com>
2022-05-25 16:07:04 -0400

    Basic time blocks (v1.1)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,30 @@
-var linkTest = $('#currentDay');
+let currentDay = $('#currentDay');
+let container = $('.container');
+let timeBlock = $('<div></div>');
 
-linkTest.text("This is a test");
+timeBlock.addClass('time-block');
+
+
+// Display current day and date in heading
+currentDay.text(`Today is ${moment().format('dddd, MMMM Do YYYY')}`);
+
+// Populate time blocks in container
+createTimeBlock = (timeBlock) => {
+    for (let i = 6; i < 22; i++) {
+        timeBlock = $('<div></div>');
+        timeBlock.addClass('time-block p-5');
+        if (i < 13) {
+            console.log(i);
+            timeBlock.text(`${i}:00am`);
+            container.append(timeBlock);
+            timeBlock.attr('id', `${i}am`);
+        } else if (i >= 13) {
+            console.log(i-12);
+            timeBlock.text(`${i-12}:00pm`);
+            container.append(timeBlock);
+            timeBlock.attr('id', `${i-12}pm`);
+
+        };
+    };
+};
+createTimeBlock(timeBlock);
