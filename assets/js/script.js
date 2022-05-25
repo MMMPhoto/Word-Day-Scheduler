b616b334fa9f75313e6c23150d47662408385af5
Max McDonough <max.mcdonough@gmail.com>
2022-05-25 17:17:23 -0400

    Added time block components (v1.2)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 let currentDay = $('#currentDay');
 let container = $('.container');
-let timeBlock = $('<div></div>');
-
-timeBlock.addClass('time-block');
-
+let currentHour = moment().hour(); 
+let row;
+let timeBlock;
+let hour;
+let textBox;
+let button;
 
 // Display current day and date in heading
 currentDay.text(`Today is ${moment().format('dddd, MMMM Do YYYY')}`);
@@ -11,20 +13,38 @@ currentDay.text(`Today is ${moment().format('dddd, MMMM Do YYYY')}`);
 // Populate time blocks in container
 createTimeBlock = (timeBlock) => {
     for (let i = 6; i < 22; i++) {
+        row = $('<div></div>');
+        row.addClass('row');
         timeBlock = $('<div></div>');
-        timeBlock.addClass('time-block p-5');
+        timeBlock.addClass('time-block row w-100');
+        container.append(row);
+        row.append(timeBlock);
+        hour = $('<div></div>');
+        hour.addClass('hour col-2 pt-2');
+        timeBlock.append(hour);
+        textBox = $('<div></div>');
+        textBox.addClass('col-8');
+        timeBlock.append(textBox);
+        button = $('<button></button>');
+        button.addClass('saveBtn col-2');
+        timeBlock.append(button);
         if (i < 13) {
             console.log(i);
-            timeBlock.text(`${i}:00am`);
-            container.append(timeBlock);
+            hour.text(`${i}:00am`);
             timeBlock.attr('id', `${i}am`);
         } else if (i >= 13) {
             console.log(i-12);
-            timeBlock.text(`${i-12}:00pm`);
-            container.append(timeBlock);
-            timeBlock.attr('id', `${i-12}pm`);
-
+            hour.text(`${i-12}:00pm`);
+            timeBlock.attr('id', `${i-12}pm`);            
+        };
+        if (i < currentHour) {
+            textBox.addClass('past');
+        } else if (i == currentHour) {
+            textBox.addClass('present');
+        } else if (i > currentHour) {
+            textBox.addClass('future');
         };
     };
 };
+
 createTimeBlock(timeBlock);
